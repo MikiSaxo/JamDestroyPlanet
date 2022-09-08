cf4e1aeeeae58067bd48b51e6e6c150c514ff2c6
P3D\gduhamel <gatien.duhamel@lacatholille.fr>
2022-09-08 15:14:30 +0200

    Values GameManager

--- Assets/Scripts/gamemanager.cs
+++ Assets/Scripts/gamemanager.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private int years = 0;
+    [SerializeField] private int population;
+    [SerializeField] [Range(0, 100)] private float devellopmentPercentage;
 
     private void Start()
     {
-        
+        PlusYear();
     }
 
     void PlusYear()
     {
         years++;
     }
+
+    void NextTurn()
+    {
+
+    }
+
 }
