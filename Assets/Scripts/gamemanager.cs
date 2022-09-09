5629b854fb0bb9457c0e41ae71f526cc95eb23e8
Hugo_Vanderhaegen <hugo.vanderhaegen@student.junia.com>
2022-09-09 10:24:29 +0200

    Generation card

--- Assets/Scripts/gamemanager.cs
+++ Assets/Scripts/gamemanager.cs
@@ -8,13 +8,15 @@ public class GameManager : MonoBehaviour
     [SerializeField] private int years = 0;
     [SerializeField] private int population = 1;
     [SerializeField] [Range(0, 100)] private float devellopmentPercentage = 0;
-
+    
     public TextMeshProUGUI yearsTxt;
     public TextMeshProUGUI populationTxt;
-
-
+    [SerializeField] Transform canvasTrasform;
+    [SerializeField] private List<CardData> cards;
+    [SerializeField] private const int drawCount = 3;
     private void Start()
     {
+        NewCards();
         PlusYear();
     }
 
@@ -29,8 +31,31 @@ public class GameManager : MonoBehaviour
     }
 
     //Create cards for the next Turn
-    void NewCards(List<Card> Cards)
+    void NewCards()
     {
+        Canvas canvas = canvasTrasform.gameObject.GetComponent<Canvas>();
+        Debug.Log(canvas.renderingDisplaySize.x);
+        List<CardData> drawableCards = cards;
+        for (int i = 0; i < drawCount; i++)
+        {
+            int index = Random.Range(0, drawableCards.Count);
+            CardData GeneratedCard = drawableCards[index];
+            drawableCards.Remove(GeneratedCard);
+            GameObject cardObject = Instantiate(GeneratedCard.Prefab, canvasTrasform);
+            Card card = cardObject.GetComponent<Card>();
+            card.Init(GeneratedCard);
+            float xPosition = 0;
+            if (drawCount % 2 == 0)
+            {
+                xPosition = canvas.renderingDisplaySize.x / (drawCount + 2) * (i + 1);
+               
+            }
+            else
+            {
+                xPosition = canvas.renderingDisplaySize.x / (drawCount + 1) * (i + 1);
+            }
+            cardObject.transform.position = new Vector3(xPosition, canvas.renderingDisplaySize.y / 2, 0);
+        }
 
     }
 
