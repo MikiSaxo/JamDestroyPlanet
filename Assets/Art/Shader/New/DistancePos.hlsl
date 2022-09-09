a21c8df3aa8fd0df915bb4e09f428343c039805e
P3D\Tlecorvic <tom.le-corvic@lacatholille.fr>
2022-09-09 17:19:10 +0200

    Push 3D

---
+++ Assets/Art/Shader/New/DistancePos.hlsl
@@ -0,0 +1,18 @@
+﻿void DistancePos_half(in float3 playerPos, in float3 worldPos, in float radius, in float3 primaryTexture, in float3 secondaryTexture,in float4 ColorFresnel, in float fresnelThickness, out float3 Out)
+{
+	if(distance(playerPos.xyz, worldPos.xyz) > radius)
+	{
+		Out = primaryTexture;
+	}
+	else if (distance(playerPos.xyz, worldPos.xyz) > radius - fresnelThickness)
+	{
+		Out = ColorFresnel;
+	}
+	else
+	{
+		Out = secondaryTexture;
+	}
+
+
+
+}
